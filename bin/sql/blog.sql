49392bf60e0286fa61cfea4a6c6bdc0a5d919ac6
Nasar <NasarKamish@gmail.com>
2021-10-27 08:46:45 +0200

    Updated file

--- bin/sql/blog.sql
+++ bin/sql/blog.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
     id serial,
     name character varing(50),
     surname character varing(50),
+    username character varing(50),
     email character varing(50),
     password character varing(50),
 );
@@ -24,7 +25,7 @@ CREATE TABLE savedblogs (
     blog_id int,
 );
 
-CREATE TABLE userblog (
+CREATE TABLE userblogs (
     user_id int,
     blog_id int,
 );
@@ -49,28 +50,28 @@ CREATE TABLE userblog (
 
 -- -- -- -- -- -- INSERT INTO TABLES -- -- -- -- -- --
 
-INSERT INTO monsters(name, personality)
-VALUES
-('The World', 'aggressive'),
-('Star Platinum', 'aggressive'),
-('Silver Chariot', 'impatient'),
-('Stone Free', 'impatient'),
-('Limp Bizkit', 'passive'),
-('Killer Queen', 'aggressive'),
-('Deadly Queen', 'passionate');
+-- INSERT INTO monsters(name, personality)
+-- VALUES
+-- ('The World', 'aggressive'),
+-- ('Star Platinum', 'aggressive'),
+-- ('Silver Chariot', 'impatient'),
+-- ('Stone Free', 'impatient'),
+-- ('Limp Bizkit', 'passive'),
+-- ('Killer Queen', 'aggressive'),
+-- ('Deadly Queen', 'passionate');
 
-INSERT INTO habitats(name, climate, temperature)
-VALUES
-('desert', 'dry', 100),
-('forrest', 'haunted', 70),
-('mountain', 'icy', 30);
+-- INSERT INTO habitats(name, climate, temperature)
+-- VALUES
+-- ('desert', 'dry', 100),
+-- ('forrest', 'haunted', 70),
+-- ('mountain', 'icy', 30);
 
-INSERT INTO lives(monster, habitat)
-VALUES
-('The World', 'desert'),
-('Star Platinum', 'desert'),
-('Silver Chariot', 'desert'),
-('Stone Free', 'mountain'),
-('Limp Bizkit', 'mountain'),
-('Killer queen', 'forrest'),
-('Deadly queen', 'forrest');
+-- INSERT INTO lives(monster, habitat)
+-- VALUES
+-- ('The World', 'desert'),
+-- ('Star Platinum', 'desert'),
+-- ('Silver Chariot', 'desert'),
+-- ('Stone Free', 'mountain'),
+-- ('Limp Bizkit', 'mountain'),
+-- ('Killer queen', 'forrest'),
+-- ('Deadly queen', 'forrest');
