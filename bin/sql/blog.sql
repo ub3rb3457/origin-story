19206e5ea1fb4b171995459ee29e202f90a355c0
Nasar <NasarKamish@gmail.com>
2021-10-27 12:15:31 +0200

    Updated file

--- bin/sql/blog.sql
+++ bin/sql/blog.sql
@@ -44,45 +44,45 @@ CREATE TABLE authorblogs (
 -- -- -- -- -- -- INSERT INTO TABLES -- -- -- -- -- --
 
 
--- INSERT INTO users(username, email, type, password)
--- VALUES
--- ('JPJPJP', 'JPeter@gmail.com', 'Author', '123456789'),
--- ('SPSPSP', 'SParker@gmail.com', 'Author', 'qwerty'),
--- ('PMPMPM', 'PMan@gmail.com', 'Reader', 'ytrewq');
+INSERT INTO users(username, email, type, password)
+VALUES
+('JPJPJP', 'JPeter@gmail.com', 'Author', '123456789'),
+('SPSPSP', 'SParker@gmail.com', 'Author', 'qwerty'),
+('PMPMPM', 'PMan@gmail.com', 'Reader', 'ytrewq');
 
--- INSERT INTO blogs(title, content)
--- VALUES
--- ('The beach', 'Finally at the beach ready to pppaaaarrrrttttyyyy!!'),
--- ('Local loto game update', 'There has been a winner for the 10 thousand Rand airtime, and his name is Thabo!'),
--- ('Thabo update', 'The local loto winner Thabo has been disapointed in his win.');
+INSERT INTO blogs(title, content)
+VALUES
+('The beach', 'Finally at the beach ready to pppaaaarrrrttttyyyy!!'),
+('Local loto game update', 'There has been a winner for the 10 thousand Rand airtime, and his name is Thabo!'),
+('Thabo update', 'The local loto winner Thabo has been disapointed in his win.');
 
--- INSERT INTO comments(user_id, blog_id, content)
--- VALUES
--- ('1', '1', 'Can i join?'),
--- ('2', '2', 'That is so nice!!!!'),
--- ('3', '3', '!XD!XD!XD!XD!XD!');
+INSERT INTO comments(user_id, blog_id, content)
+VALUES
+('1', '1', 'Can i join?'),
+('2', '2', 'That is so nice!!!!'),
+('3', '3', '!XD!XD!XD!XD!XD!');
 
--- INSERT INTO savedblogs(user_id, blog_id)
--- VALUES
--- ('1', '2'),
--- ('1', '3'),
--- ('2', '1'),
--- ('3', '1');
+INSERT INTO savedblogs(user_id, blog_id)
+VALUES
+('1', '2'),
+('1', '3'),
+('2', '1'),
+('3', '1');
 
--- INSERT INTO likedblogs(user_id, blog_id)
--- VALUES
--- ('1', '2'),
--- ('2', '1'),
--- ('3', '1');
+INSERT INTO likedblogs(user_id, blog_id)
+VALUES
+('1', '2'),
+('2', '1'),
+('3', '1');
 
--- INSERT INTO dislikedblogs(user_id, blog_id)
--- VALUES
--- ('1', '3'),
--- ('2', '3'),
--- ('3', '2');
+INSERT INTO dislikedblogs(user_id, blog_id)
+VALUES
+('1', '3'),
+('2', '3'),
+('3', '2');
 
--- INSERT INTO authorblogs(user_id, blog_id)
--- VALUES
--- ('1', '1'),
--- ('2', '2'),
--- ('2', '3');
+INSERT INTO authorblogs(user_id, blog_id)
+VALUES
+('1', '1'),
+('2', '2'),
+('2', '3');
