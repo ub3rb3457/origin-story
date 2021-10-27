dff49513d09329f7622096b90974ebb504c05040
Nasar <NasarKamish@gmail.com>
2021-10-27 11:20:59 +0200

    Updated file

--- bin/sql/blog.sql
+++ bin/sql/blog.sql
@@ -2,8 +2,6 @@
 
 CREATE TABLE users (
     id serial,
-    name character varying(50),
-    surname character varying(50),
     username character varying(50),
     email character varying(50),
     type character varying(50),
@@ -46,17 +44,50 @@ CREATE TABLE userblogs (
 -- -- -- -- -- -- INSERT INTO TABLES -- -- -- -- -- --
 
 
--- INSERT INTO users(name, surname, username, email, type, password)
+-- INSERT INTO users(username, email, type, password)
+-- VALUES
+-- ('JPJPJP', 'JPeter@gmail.com', 'Author', '123456789'),
+-- ('SPSPSP', 'SParker@gmail.com', 'Author', 'qwerty'),
+-- ('PMPMPM', 'PMan@gmail.com', 'Reader', 'ytrewq');
+
+-- -- -- -- -- -- --CREATE TABLE blogs (
+-- -- -- -- -- -- --    id serial,
+-- -- -- -- -- -- --    title character varying(50),
+-- -- -- -- -- -- --    content character varying(50)
+-- -- -- -- -- -- --);
+
+-- INSERT INTO blogs(name, surname, username, email, type, password)
 -- VALUES
 -- ('John', 'Peter', 'JPJPJP', 'JPeter@gmail.com', 'Author', '123456789'),
 -- ('Spider', 'Parker', 'SPSPSP', 'SParker@gmail.com', 'Author', 'qwerty'),
 -- ('Peter', 'Man', 'PMPMPM', 'PMan@gmail.com', 'Reader', 'ytrewq');
 
--- INSERT INTO users(name, surname, username, email, type, password)
--- VALUES
--- ('John', 'Peter', 'JPJPJP', 'JPeter@gmail.com', 'Author', '123456789'),
--- ('Spider', 'Parker', 'SPSPSP', 'SParker@gmail.com', 'Author', 'qwerty'),
--- ('Peter', 'Man', 'PMPMPM', 'PMan@gmail.com', 'Reader', 'ytrewq');
+-- CREATE TABLE comments (
+--     id serial,
+--     user_id int,
+--     blog_id int,
+--     content character varying(50)
+-- );
+
+-- -- -- -- -- -- -- CREATE TABLE savedblogs (
+-- -- -- -- -- -- --     user_id int,
+-- -- -- -- -- -- --     blog_id int
+-- -- -- -- -- -- -- );
+
+-- -- -- -- -- -- -- CREATE TABLE likedblog (
+-- -- -- -- -- -- --     user_id int,
+-- -- -- -- -- -- --     blog_id int
+-- -- -- -- -- -- -- );
+
+-- -- -- -- -- -- -- CREATE TABLE dislikedblog (
+-- -- -- -- -- -- --     user_id int,
+-- -- -- -- -- -- --     blog_id int
+-- -- -- -- -- -- -- );
+
+--  -- -- -- -- -- -- CREATE TABLE userblogs (
+--  -- -- -- -- -- --     user_id int,
+--  -- -- -- -- -- --     blog_id int
+--  -- -- -- -- -- -- );
 
 -- INSERT INTO habitats(name, climate, temperature)
 -- VALUES
