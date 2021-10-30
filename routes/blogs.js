b5969aa59743a29053791d500785f72dcb22dfa0
Nasar <NasarKamish@gmail.com>
2021-10-30 14:09:26 +0200

    Updated file

--- routes/blogs.js
+++ routes/blogs.js
@@ -4,6 +4,17 @@ const { Router } = require("express");
 const pool = require("../db");
 const router = Router();
 
+// // // // // // ROUTER USES // // // // // //
+
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // // // // // // GET METHOLDS // // // // // //
 
 router.get("/", (request, response, next) => {
