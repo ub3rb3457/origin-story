682fc760c04f02bc26fba594b668c1e1fe7abc28
ub3rb3457 <perchance2dream86@gmail.com>
2022-03-06 05:28:24 -0500

    added vue-router along with several other packages

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
+import router from './router'
 
-createApp(App).use(Quasar, quasarUserOptions).mount("#app");
+const myApp = createApp(App)
+myApp.use(router)
+myApp.use(Quasar, quasarUserOptions).mount("#app");
